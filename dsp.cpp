36306505885c3ecfbb6b1519f8c015d27dfac3f7
seandorian <sean@sprouts.io>
2015-05-26 20:33:33 -0400

    IT MAKES A SINE.

--- dsp.cpp
+++ dsp.cpp
@@ -1,7 +1,17 @@
 
 #include "DSPController.hpp"
 
+void dumpBufferOfSize( const float * buffer, const int bufferSize ) {
 
+    if (buffer != nullptr) {
+
+        std::for_each(&buffer[0], &buffer[bufferSize - 1], [] (float sample) {
+            std::cout << sample << "\n";
+        });
+
+    }
+
+}
 
 
 int main(int argc, char const *argv[]) {
@@ -10,7 +20,6 @@ int main(int argc, char const *argv[]) {
     // Obligatory parameters when working with DSP. :)
     const uint BUFFER_SIZE = 2048;
     const float SAMPLE_RATE = 44100.0;
-
 
     // The DSPCallbackController is given a list of callbacks to execute in the order received, and will report its state as it executes.
     DSPModuleController<float> moduleController = DSPModuleController<float>(SAMPLE_RATE, BUFFER_SIZE);
@@ -33,18 +42,30 @@ int main(int argc, char const *argv[]) {
 
     };
 
-    moduleController.addDSPCallback(randomNoiseGenerator, nullptr);
-    // moduleController.addDSPCallback(incrementSample);
 
-    float * buffer = moduleController.renderCallbackChain();
-    if (buffer != nullptr) {
+    DSPCallbackFloat sineGenerator = [] (DSPCallbackStateFloat *callbackState, DSPCallbackParametersFloat *callbackParameters) -> void {
 
-        std::for_each(&buffer[0], &buffer[BUFFER_SIZE - 1], [] (float sample) {
-            std::cout << "renderCallbackChain Output: " << sample << "\n";
-        });
+        const float TWOPI = (2 * 3.14159);
+        static float frequency = 440.0;
+        static float phase = 0.0;
+        static float phaseIncrement = (TWOPI * (44100.0 / frequency)) / callbackState->bufferSize;
 
-    }
+        for (int i = 0; i < callbackState->bufferSize; ++i)
+        {
 
+            float sample = sin(phase);
+            // Write the sample to the buffer.
+            callbackState->buffer[i] = sample;
+            phase += phaseIncrement;
+
+        }
+
+
+    };
+
+    moduleController.addDSPCallback(sineGenerator, nullptr);
+
+    dumpBufferOfSize(moduleController.renderCallbackChain(), BUFFER_SIZE);
 
     return 0;
 }
